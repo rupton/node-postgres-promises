dcafdcb2d50ca471a168a2a40479f3fd37227a91
Ryan Upton <ryan.upton@ryanupton-ltm.internal.salesforce.com>
2017-05-12 12:20:44 -0400

    prepped for Heorku

--- queries.js
+++ queries.js
@@ -5,15 +5,30 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://localhost:5432/puppies';
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/puppies';
+console.log(connectionString);
 var db = pgp(connectionString);
 
 //add query functions
 
 module.exports = {
 	getAllPuppies: getAllPuppies,
-	getSinglePuppy; getSinglePuppy,
-	createPuppy: createPuppy,
-	updatePuppy: updatePuppy,
-	removePuppy: removePuppy;
+	//getSinglePuppy: getSinglePuppy,
+	//createPuppy: createPuppy,
+	//updatePuppy: updatePuppy,
+	//removePuppy: removePuppy
 };
+function getAllPuppies(req, res, next) {
+  db.any('select * from pups')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL puppies'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
